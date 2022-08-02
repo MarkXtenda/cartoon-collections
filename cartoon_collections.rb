98fed98df98e60e813d42067ce62dbd099bdd77a
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-02 21:31:06 +1000

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |dwarf, index| 
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(aaaa)
+  aaaa.map do |a|
+    a.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|element| element.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_ingredients)
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_found = nil
+  cheese_types.map do |cheese|
+    (array_ingredients.include?(cheese)) ? cheese_found = cheese : nil
+  end
+  (cheese_found != nil) ? cheese_found : nil
 end
